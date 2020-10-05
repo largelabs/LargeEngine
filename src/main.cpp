43af28f3d747137e6524cf066fc628a38393d7b2
Gaël THEROND <noreply@github.bitswalk.com>
2020-10-05 16:06:14 +0200

    Add .vscode config for build tasks. Add .vscode config for c++Properties file. Use space over tabs.

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,29 @@
 
 int main()
 {
-	std::cout << "Initializing LE environment:\n";
-	glfwInit();
+  std::cout << "Initializing LE environment:\n";
+  glfwInit();
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-	std::cout << extensionCount << " extensions supported\n";
+  std::cout << extensionCount << " extensions supported\n";
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
 
-	while(!glfwWindowShouldClose(window)) {
-			glfwPollEvents();
-	}
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+  }
 
-	glfwDestroyWindow(window);
+  glfwDestroyWindow(window);
 
-	glfwTerminate();
+  glfwTerminate();
 
-	return 0;
+  return 0;
 }
